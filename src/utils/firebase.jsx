c9bc3d17b93d9400d79847f495cfb5707b10cf3d
Eshwar-B <1728eshwar@gmail.com>
2025-02-18 18:47:24 +0530

    secured api keys

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -10,12 +10,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCnrMgGKaOvuuJWGbrxD-bSVNkh9kERRnk",
-  authDomain: "netflix-gpt-b1191.firebaseapp.com",
-  projectId: "netflix-gpt-b1191",
-  storageBucket: "netflix-gpt-b1191.firebasestorage.app",
-  messagingSenderId: "1086082078592",
-  appId: "1:1086082078592:web:4d0d3880c3dd1750b41365",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-348DH8R39Q"
 };
 
